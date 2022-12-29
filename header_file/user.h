4c993399d2a8a3a41c735ffac5eee40845360f9c
citronik <perdoch.filip@gmail.com>
2022-12-29 19:16:23 +0100

    User structure

--- header_file/user.h
+++ header_file/user.h
@@ -5,4 +5,17 @@
 #ifndef SEMESTRALNA_PRACA_UDSP_USER_H
 #define SEMESTRALNA_PRACA_UDSP_USER_H
 
+#define USER_NAME_LENGTH 50
+#define USER_PASSWORD_LENGTH 20
+#define USER_EMAIL_LENGTH 70
+
+typedef struct user {
+    char user_email_[USER_EMAIL_LENGTH];
+    char first_name_[USER_NAME_LENGTH];
+    char last_name_[USER_NAME_LENGTH];
+    char password_[USER_PASSWORD_LENGTH];
+    int id_;
+} USER;
+
+
 #endif //SEMESTRALNA_PRACA_UDSP_USER_H
---
+++ source_file/user.c
@@ -0,0 +1,4 @@
+//
+// Created by Filip on 29. 12. 2022.
+//
+#include "../header_file/user.h"
