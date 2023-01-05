9d13954ba02cdd08fc0276f63ffe4b5e7e22df22
Slavomír Tatarka <slavic112@gmail.com>
2023-01-05 18:13:14 +0100

    adding functions for component in registration system

--- header_file/registration_system.h
+++ header_file/registration_system.h
@@ -11,8 +11,10 @@
 #define BUFFER 150
 typedef struct registration_system {
     USER users_[CAPACITY];
+    COMPONENT components_[CAPACITY];
+    unsigned int number_of_components;
     unsigned int number_of_users_;
-    //double earnings;
+    double earnings;
 } REGISTRATION_SYSTEM;
 
 //void registration_system_init(REGISTRATION_SYSTEM *rs);
@@ -20,9 +22,13 @@ void * registration_system_start(void * data);
 USER* add_user(REGISTRATION_SYSTEM *rs, USER *us); //adding existing user
 USER* remove_user(REGISTRATION_SYSTEM *rs, USER *us); // removing existing user (only from reg.system)
 void print_users(const REGISTRATION_SYSTEM *rs); // prints users in the registration system
-_Bool create_user(REGISTRATION_SYSTEM *rs); // create new user
+void print_components(const REGISTRATION_SYSTEM *rs);
+_Bool registrate_user(REGISTRATION_SYSTEM *rs); // create new user
 _Bool delete_user(REGISTRATION_SYSTEM *rs); //complete delete of user
 USER * find_user(REGISTRATION_SYSTEM *rs); // finds the user in the registration system
+COMPONENT * find_component(REGISTRATION_SYSTEM  *rs);
 void reg_sys_to_string(REGISTRATION_SYSTEM *rs); // prints details about the registration system
-
+COMPONENT* add_component(REGISTRATION_SYSTEM *rs, COMPONENT *cp); // adding component
+COMPONENT* remove_component(REGISTRATION_SYSTEM *rs, COMPONENT *cp);  // removing component
+_Bool registrate_component(REGISTRATION_SYSTEM *rs);
 #endif //SEMESTRALNA_PRACA_UDSP_REGISTRATION_SYSTEM_H
